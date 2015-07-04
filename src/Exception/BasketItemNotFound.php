ee5bcbde4fa0a6a508cf09c1fa0f1caf93043926
Sergey <skyeff@gmail.com>
2015-07-04 18:02:18 +0300

    - fixed: BasketItemNotFound constructor;

--- src/Exception/BasketItemNotFound.php
+++ src/Exception/BasketItemNotFound.php
@@ -6,7 +6,7 @@ use BSA2015\Basket\BasketItem;
 use Exception;
 
 class BasketItemNotFound extends Exception {
-    public function __construct(BasketItem $basketItem = "", $code = 0, Exception $previous = null)
+    public function __construct(BasketItem $basketItem = null, Exception $previous = null)
     {
         parent::__construct('Basket Item #' . $basketItem->getId() . ' not found', 404, $previous);
     }
